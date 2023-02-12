0e0d75b2693047c9d8edc8805abd08669b343ffc
kirhal <kirillgaleckiy@gmail.com>
2023-02-12 18:02:07 +0200

    1

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,23 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const galleryBoxEl = document.querySelector(".gallery");
+
+function createListItemsMarkup(obj) {
+  return obj
+    .map(
+      ({ preview, original, description }) => `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"      
+      alt="${description}"
+    />
+  </a>
+</div>`
+    )
+    .join("");
+}
+
+galleryBoxEl.innerHTML = createListItemsMarkup(galleryItems);
+
+galleryBoxEl.addEventListener("click", onGalleryBoxClick);
