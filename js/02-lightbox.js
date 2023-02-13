b78f3f30d374edfb54a66785fcfe0fbd65bc36b5
kirhal <kirillgaleckiy@gmail.com>
2023-02-13 16:46:58 +0200

    finish

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -5,29 +5,21 @@ const galleryBoxEl = document.querySelector(".gallery");
 function createListItemsMarkup(obj) {
   return obj
     .map(
-      ({ preview, original, description }) => `<div class="gallery__item">
-  <a class="gallery__link" href="${original}">
-    <img
-      class="gallery__image"
-      src="${preview}"      
-      alt="${description}"
-    />
-  </a>
-</div>`
+      ({ preview, original, description }) => `
+<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`
     )
     .join("");
 }
 
 galleryBoxEl.innerHTML = createListItemsMarkup(galleryItems);
 
-galleryBoxEl.addEventListener("click", onGalleryBoxClick);
-
-function onGalleryBoxClick(e) {
-  e.preventDefault();
-  if (e.target.nodeName !== "IMG") {
-    return;
-  }
-  var lightbox = new SimpleLightbox(".gallery a", {
-    /* options */
-  });
-}
+let gallery = new SimpleLightbox(".gallery a", {
+  captionDelay: 250,
+  scrollZoom: false,
+  captionType: "attr",
+  captionSelector: "img",
+  captionsData: "alt",
+  docClose: false,
+});
